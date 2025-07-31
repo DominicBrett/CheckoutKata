8963e3416c8b4ef38b596a30f0630116a4bbf5e7
DominicBrett <dombrett777@outlook.com>
2025-07-31 19:56:43 +0100

    Adds more test for Scan()

--- CheckoutKata.Tests/CheckoutTest.cs
+++ CheckoutKata.Tests/CheckoutTest.cs
@@ -27,5 +27,52 @@ namespace CheckoutKata.Tests
             Assert.Equal(1, item.Quantity);
 
         }
+        [Fact]
+        public void Scan_ShouldTrackQuanitityOverMultipleCalls()
+        {
+            // Arrange
+            var expectedQuantity = 33;
+            var checkout = new Checkout();
+
+            // Act
+            for (int i = 0; i < expectedQuantity; i++)
+            {
+                checkout.Scan("A");
+            }
+            var item = checkout.GetItemFromBasket("A");
+
+            // Assert
+            Assert.NotNull(item);
+            Assert.Equal("A", item.Sku);
+            Assert.Equal(50, item.Price);
+            Assert.Equal(expectedQuantity, item.Quantity);
+
+        }
+        [Fact]
+        public void Scan_ShouldHandleMultipleUniqueItems()
+        {
+            // Arrange
+            var checkout = new Checkout();
+
+            // Act
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("B");
+
+            var item1 = checkout.GetItemFromBasket("A");
+            var item2 = checkout.GetItemFromBasket("B");
+
+            // Assert
+            Assert.NotNull(item1);
+            Assert.Equal("A", item1.Sku);
+            Assert.Equal(50, item1.Price);
+            Assert.Equal(1, item1.Quantity);
+
+            Assert.NotNull(item2);
+            Assert.Equal("B", item2.Sku);
+            Assert.Equal(30, item2.Price);
+            Assert.Equal(2, item2.Quantity);
+
+        }
     }
 }
