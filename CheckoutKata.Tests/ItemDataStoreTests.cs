c098072148c61e5c96b1cb234a1d7fd2ceb702fb
DominicBrett <dombrett777@outlook.com>
2025-07-31 21:01:00 +0100

    adds tests method for a new method which will add or update items with an item object

--- CheckoutKata.Tests/ItemDataStoreTests.cs
+++ CheckoutKata.Tests/ItemDataStoreTests.cs
@@ -1,3 +1,4 @@
+using CheckoutKata.Entities;
 using CheckoutKata.Services;
 
 namespace CheckoutKata.Tests
@@ -48,6 +49,37 @@ namespace CheckoutKata.Tests
             var item = _itemDataStore.GetItemItem(expectedItemSku);
             Assert.Equal(expectedItemPrice, item.Price);
             Assert.Equal(expectedItemSku, item.Sku);
+        }
+
+        [Fact]
+        public void AddOrUpdateItem_ShouldAddANewItem_IfItDosentAlreadyExsistInTheDataStore()
+        {
+            // Arrange
+            var expectedItem = new Item() { Price = 1, Sku = "D", Promotion = new Promotion() { Price = 1, QuantityRequirement = 1 } };
+
+            // Act
+            _itemDataStore.AddOrUpdateItem(expectedItem);
+
+            // Assert
+            var item = _itemDataStore.GetItemItem(expectedItem.Sku);
+            Assert.Equal(expectedItem.Price, item.Price);
+            Assert.Equal(expectedItem.Sku, item.Sku);
+            Assert.Equal(expectedItem.Promotion, item.Promotion);
+        }
+        [Fact]
+        public void AddOrUpdateItem_ShouldUpdateExsistingItem_IfItAlreadyExsistInTheDataStore()
+        {
+            // Arrange
+            var expectedItem = new Item() { Price = 1, Sku = "D", Promotion = new Promotion() { Price = 1, QuantityRequirement = 1 } };
+
+            // Act
+            _itemDataStore.AddOrUpdateItem(expectedItem);
+
+            // Assert
+            var item = _itemDataStore.GetItemItem(expectedItem.Sku);
+            Assert.Equal(expectedItem.Price, item.Price);
+            Assert.Equal(expectedItem.Sku, item.Sku);
+            Assert.Equal(expectedItem.Promotion, item.Promotion);
         }
     }
 }
